b7b62567d383c6d398143694f6119a1dcb8b5e30
Alexander Danilovsky <alex.dllsky@gmail.com>
2019-03-21 16:06:32 +0300

    Усовершенствовал модуль шифрования данных

--- Assets/Scripts/DllSky/Protection/SimpleEncrypting.cs
+++ Assets/Scripts/DllSky/Protection/SimpleEncrypting.cs
@@ -6,14 +6,42 @@ namespace DllSky.Protection
     //Base64 преобразование
     public static class SimpleEncrypting
     {
+        public static string Revert(this string _str)
+        {
+            var result = "";
+
+            for (int i = _str.Length-1; i >= 0; i--)
+            {
+                result += _str[i];
+            }
+
+            return result;
+        }
+
+        public static string RandomChar(this string _str)
+        {
+            if (string.IsNullOrEmpty(_str))
+                return "a";
+
+            var symbol = _str.ToCharArray(UnityEngine.Random.Range(0, _str.Length), 1);
+
+            return symbol[0].ToString();
+        }
+
         /// <summary>
         /// "Шифрование". Преобразуем в Base64
         /// </summary>
         public static string Encode(string _commonText)
         {
+            //Байты
             var plainTextBytes = Encoding.UTF8.GetBytes(_commonText);
+            //Строка64
+            var simpleEncode = Convert.ToBase64String(plainTextBytes);
+            //Обратная строка
+            var revertEncode = simpleEncode.Revert();
 
-            return Convert.ToBase64String(plainTextBytes);
+            //Возвращяем со случайным первым символом
+            return revertEncode.Insert(0, revertEncode.RandomChar());
         }
 
         /// <summary>
@@ -21,9 +49,16 @@ namespace DllSky.Protection
         /// </summary>
         public static string Decode(string _base64Text)
         {
-            var base64EncodedBytes = Convert.FromBase64String(_base64Text);
+            //Шифрованная обратная строка без первого символа
+            _base64Text = _base64Text.Remove(0, 1);
+            //Прямая зашифрованная строка
+            var revertDecode = _base64Text.Revert();
+            //Байты обратной зашифрованой строки (убираем первый символ, разворачиваем)
+            var base64EncodedBytes = Convert.FromBase64String(revertDecode);
+            //Расшифрованная строка
+            var simpleDecode = Encoding.UTF8.GetString(base64EncodedBytes);
 
-            return Encoding.UTF8.GetString(base64EncodedBytes);
+            return simpleDecode;
         }
     }
 }
